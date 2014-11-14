96b7bb3c3a4cfbb02af35888a9a75c28d24b56b1
Chandler <mew1033@users.noreply.github.com>
2014-11-13 23:30:42 -0700

    Update setupIptables.sh

--- Linux/portspoof/setupIptables.sh
+++ Linux/portspoof/setupIptables.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo -n "Input the ports you want to allow (ie: 22,21,5000:5500"
+echo -n "Input the ports you want to allow (ie: 22,21,5000:5500): "
 read ports
 iptables -t nat -I PREROUTING -p tcp -m tcp --match multiport --dports $ports -j ACCEPT
 iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 1:65535 -j REDIRECT --to-ports 4444
